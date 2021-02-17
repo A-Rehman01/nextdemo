9c8143b5fc71f15571600713b9b29560a6519f42
abdulrehman <arehmana377@gmail.com>
2021-02-17 14:34:43 +0500

    Update

---
+++ pages/Person1.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Person1 = () => {
+  return <div>PersonFile</div>;
+};
+
+export default Person1;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
-
+import Person1 from './Person1';
 export default function Home() {
   return (
     <div className={styles.container}>
       Main Branch
       <div>Person1</div>
+      <Person1 />
     </div>
   );
 }
