ba745f0e2b83329ef4a9d9a7bf76f4d9d6516d44
abdulrehman <arehmana377@gmail.com>
2021-02-17 14:40:14 +0500

    Update

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import Person3 from './Person3';
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       Main Branch
       <div>Person3</div>
       <div>Person1</div>
+      <Person3 />
     </div>
   );
 }
